e6630a80609bcb3fcd285f60c807ec80c7be385a
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-08-24 10:07:03 +0530

    files updated

---
+++ 02.Variables/variable.py
@@ -0,0 +1,29 @@
+name= "Shrushti"
+age= 18
+city= "Kalol"
+
+student_name= "Shrushti"
+student_roll_number= 12
+student_branch= "CSE"
+
+marks= 75
+print(marks)
+marks= 85 
+print(marks)
+
+name, age, city= "Rahul", 18, "Patna"
+print(name,age,city)
+
+x=y=z=0
+print(x,y,z)
+
+student_name= "Rahul"
+student_full_name= "Rahul"
+student_class= "B.Tech"
+
+#Store and update student's information
+student_name="Shrushti"
+student_age= 17
+student_age= 18 
+print("Student Name:", student_name)
+print("Student Age:", student_age)
