9c2a11042ec016a9a2f77b169fd8fe769faffae1
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-08-25 10:11:41 +0530

    added 3

---
+++ 03.DataTypes & TypeCasting/datatype.py
@@ -0,0 +1,45 @@
+student_name="Shrushti"
+student_age=18
+student_height= 4.11
+is_student= True
+student_result= None
+
+print(type(student_name))
+print(type(student_age))
+print(type(student_height))
+print(type(is_student))
+print(type(student_result))
+
+a=50
+b=50.0
+c="50"
+print(type(a))
+print(type(b))
+print(type(c))
+
+a= True
+b= "True"
+print(type(a))
+print(type(b))
+
+value= 12
+print(type(value))
+value="12"
+print(type(value))
+
+#Stored Product Information 
+product_name="Laptop"
+product_quantity=2
+product_price=76500.2
+is_product=True
+product_discount= None
+print(type(product_name))
+print(type(product_quantity))
+print(type(product_price))
+print(type(is_product))
+print(type(product_discount))
+
+age=10
+weight=10.0
+quantity="10"
+print(type(age))
