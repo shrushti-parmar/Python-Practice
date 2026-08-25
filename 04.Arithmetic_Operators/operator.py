e47b92bfd4791487268d020dc5e254bc68c63c20
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-08-25 23:59:42 +0530

    added files

---
+++ 04.Arithmetic_Operators/operator.py
@@ -0,0 +1,43 @@
+
+print(10 + 5)
+print(10 - 5)
+print(10 * 5)
+print(10 / 5)
+print(10 // 3)
+print(10 % 3)
+print(2 ** 4)
+
+print(10 + 5 * 2)
+print((10 + 5) * 2)
+
+print(20 / 5 * 2)
+print(20 // 3 * 2)
+print(20 - 5 + 2)
+
+print(10 - (-5))
+print(-10 - 5)
+print(-10 - (-5))
+
+print(10 // 3)
+print(-10 // 3)
+print(10 // -3)
+print(-10 // -3)
+
+print(10 % 3)
+print(-10 % 3)
+print(10 % -3)
+print(-10 % -3)
+
+print(-2 ** 2)
+print((-2) ** 2)
+print(-2 ** 3)
+print((-2) ** 3)
+
+print(True + True)
+print(True + False)
+print(False * 10)
+print(True * 10)
+print(True / 2)
+
+print("Python" + "Programming")
+print("Python" * 3)
