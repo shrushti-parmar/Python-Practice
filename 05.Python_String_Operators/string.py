97ef6bc2db461c79984c14d2d08a19652b034603
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-08-27 02:15:28 +0530

    add

---
+++ 05.Python_String_Operators/string.py
@@ -0,0 +1,56 @@
+name="Shrushti"
+city_name='Mehsana'
+fav_programming_lang="""Python"""
+message="Hi Everyone, I'm Shrushti. Currently I'm in CSE branch."
+print(name)
+print(city_name)
+print(fav_programming_lang)
+print(message)
+
+#empty string
+name=""
+print(len(name))
+print(type(name))
+
+a="Python Programming"
+print(a[:])
+print(len(a))
+print(a[0])
+print(a[-1])
+print(a[2])
+print(a[-2])
+
+#positive indexing
+a="Programming"
+print(a[0])
+print(a[1])
+print(a[4])
+print(a[-1])
+
+#negative indexing
+a="Programming"
+print(a[-1])
+print(a[-2])
+print(a[-3])
+print(a[-11])
+
+
+a="Shrushti Parmar"
+print(a[0])
+print(a[-1])
+print(a[9])
+
+a="Python Programming"
+print(a[0:7])
+print(a[7:])
+print(a[:])
+print(a[:5])
+print(a[13:])
+
+a="ABCDEFGHIJKL"
+print(a[0:12:2])
+print(a[0:12:3])
+print(a[1:8:2])
+print(a[::-1])
+
+
