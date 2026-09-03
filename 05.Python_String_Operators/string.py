9ce2df7b84ca9ca35e8a0b5ecfc79478959764f4
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-09-03 09:48:38 +0530

    added string fil

--- 05.Python_String_Operators/string.py
+++ 05.Python_String_Operators/string.py
@@ -54,3 +54,159 @@ print(a[1:8:2])
 print(a[::-1])
 
 
+a= "python"
+b= a[:3].upper()
+c= a[3:].lower()
+print(b+c)
+
+
+a="Œ"
+print(a.casefold())
+b="Φ"
+print(b.casefold())
+
+
+a= "python program"
+print(a.find("program"))
+print("program" in a)
+print("john" in a)
+print(a.index("thon"))
+
+text = "Python"
+
+print(text[0])
+print(text[3])
+print(text[-1])
+print(text[-2])
+
+text = "Programming"
+
+print(text[0:4])
+print(text[3:8])
+print(text[:5])
+print(text[5:])
+
+text = "Python"
+
+print(text[::2])
+print(text[1::2])
+print(text[::-1])
+
+text = "Hello World"
+
+print(len(text))
+print(text[5])
+print(text[-1])
+
+text = "Python Programming"
+
+print("Python" in text)
+print("Java" in text)
+print("Java" not in text)
+
+text = "banana"
+
+print(text.find("a"))
+print(text.find("z"))
+print(text.count("a"))
+
+text = "Python"
+
+print(text.upper())
+print(text.lower())
+print(text.capitalize())
+print(text.title())
+print(text.swapcase())
+
+text = "I like Java"
+
+print(text.replace("Java", "Python"))
+print(text)
+
+text = "Hello"
+
+print(text + " World")
+print(text * 3)
+
+a="Python Programming"
+print(a[-5:])
+print(a[-10:])
+print(a[-1::-1])
+
+a= "Aivivobook"
+print(a[0:3])
+print(a[-3:])
+print(a[0:10:2])
+print(a[::-1])
+print(a[1:9])
+
+
+Name="Shrushti"
+Age=18
+City="Mehsana"
+Programming_Language="Python"
+print(f"My name is {Name} and I am {Age} years old. I live in {City} and my favorite programming language is {Programming_Language}.")
+print(Name+ " " + str(Age) + " " + City + " " + Programming_Language)
+
+
+symbol = "@"
+print("The string is:", symbol)
+print("Type:", type(symbol))
+print(symbol*9)
+
+a= "*"
+print(a*10)
+
+a="python programming language"
+print(a.capitalize())
+print(a.title())
+print(a.upper())
+print(a.lower())
+print(a.swapcase())
+
+
+a="Python"
+b="python"
+print(a==b)
+print(a.lower()==b.lower())
+
+text = "Python"
+text.upper()
+print(text)
+
+text = "   Python Programming   "
+print(text.strip())
+print(text.lstrip())
+print(text.rstrip())
+
+a="Python is easy to learn"
+print(a.split())
+
+words = ["Python", "is", "easy"]
+print(" ".join(words))
+
+
+t="apple,banana,mango,orange"
+print(t.split(","))
+
+Name="Shrushti"
+Age=18
+City="Mehsana"
+print(f"My name is {Name} and I'm {Age} years old. I live in {City}.")
+
+a = 10
+b = 20
+print(f"Addition is {a + b}")
+
+text = "Python"
+#print(text[20])
+
+
+
+age = 20
+#print("Age: " + str(age))
+
+text = "Python"
+print(text.index("Java"))
+
+##
