ea6836764022293608fb3d4e8c0c26dc586099d5
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-09-04 18:06:09 +0530

    added files

---
+++ 07.input_and_output/input_output.py
@@ -0,0 +1,125 @@
+#name = input("Enter your name: ")
+#print(f"My name is {name}.")
+
+#city = input("Enter your city: ")
+#print(f"Your city is {city}.")
+
+#name = input("Enter your name: ")
+#age = int(input("Enter your age: "))
+#print(f"My name is {name} and I am {age} years old.")
+
+#name = input("Enter your name: ")
+#print(type(name))
+
+#first_name = input("Enter first name: ")
+#last_name = input("Enter last name: ")
+#print(first_name, last_name)
+
+#name= input("Enter your name: ")
+#city= input("Enter your city: ")
+#college= input("Enter your college name: ")
+#print(name, city, college)
+
+
+#name1, name2 = input("Enter two names: ").split()
+#print(name1, name2)
+
+#name1, name2, name3 = input("Enter three names: ").split()
+#print(name1, name2, name3)
+
+#first_name, last_name = map(str, input("Enter first name and last name: ").split())
+#print(f"First name: {first_name}, Last name: {last_name}")
+
+
+#a,b=input("enter a and b").split()
+#a=int(a)
+#b=int(b)
+#print(f"first no.:{a} last no.:{b}")
+#print(type(a),type(b))
+
+a = "25"
+b = int(a)
+print(b) 
+
+a= "25.5"
+b= float(a)
+print(b)
+
+a= 100
+b= str(a)
+print(b)
+
+#num=int(input("enter a an integer:"))
+#print(type(num))
+
+#num=float(input("enter a float-point number:"))
+#print(type(num))
+
+#a = input("Enter a number:")
+#print(a)
+#b = input("Enter a number:")
+#print(b)
+#print(a + b)
+#right-
+#a = int(input("Enter a number:"))
+#print(a)
+#b = int(input("Enter a number:"))
+#print(b)
+#print(a + b)
+
+#corrected method
+#a = int(input("Enter first number: "))
+#b = int(input("Enter second number: "))
+#print(a + b)
+
+name = "Rahul"
+age = 20
+print(f"My name is {name} and I am {age} years old.")
+
+a = 10
+b = 20
+print(f"The sum of {a} and {b} is equal to {a+b}")
+
+#name= input("Enter your name:")
+#age= int(input("Enter your age:"))
+print(f"Your name is {name} and your age is {age}.")
+
+product_price = 75.6356721
+print(f"{product_price:.2f}")
+
+#product_name=input("Enter product name: ")
+#price= int(input("Enter product price: "))
+#quantity= int(input("Enter product quantity: "))
+#print(f"I bought {quantity} {product_name} and it costed me {price*quantity} Rs.")
+
+
+print("A", "B", "C")
+
+print("2026", "08", "19", sep="-")
+
+print("Hello", end=" ")
+print("World")
+
+#First_number=int(input("Enter first no.: "))
+#Second_number=int(input("Enter second no.: "))
+#Sum=(First_number+ Second_number)
+#print(f"The sum of {First_number} and {Second_number} is equal to {Sum}.")
+
+
+#product_price=int(input("Enter product price: "))
+#quantity= int(input("Enter quantity: "))
+#print(f"The total products cost is {product_price*quantity}. ")
+
+
+#name = input("Enter your name: ")
+#age = int(input("Enter your age: "))
+#marks= float(input("Enter your marks: "))
+#print(f"Student's name is {name} and she is {age} years old. She got {marks}.")
+
+#STUDENT INFORMATION:
+
+name= input("Enter name: ")
+age= int(input("Enter age: "))
+height= float(input("Enter height: "))
+city= input("Enter city name: ")
+print(f"The student's name is {name}. He is {age} years old. He is {height:.2f} cm tall. He lives in {city}.")
