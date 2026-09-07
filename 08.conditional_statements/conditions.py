ddcdc9f2f6896237a9de503c93808be734a34209
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-09-07 09:31:16 +0530

    Added conditions.py file

---
+++ 08.conditional_statements/conditions.py
@@ -0,0 +1,38 @@
+num= 20
+if num>10:
+    print("Greater than 10")
+
+age= 17
+if age>=18:
+    print("Adult")
+
+num= int(input("Enter number: "))
+if num>0:
+    print("Positive")
+
+marks= int(input("Enter marks: "))
+if marks >= 40:
+    print("PASS")
+
+num=int(input("Enter number: "))
+if num==0:
+    print("Zero")
+
+num=int(input("Enter number: "))
+if num>0:
+    print("POSITIVE")
+else: 
+    print("NOT POSITIVE")
+
+
+num=int(input("Enter age: "))
+if age >= 18:
+    print("Adult")
+
+
+
+name= input("Enter name: ")
+if name.startswith("S"):
+    print("True")
+
+
