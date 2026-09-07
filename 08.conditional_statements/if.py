3c26d9436fa6a9cf50f8e8e3a74b940dc52baf3a
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-09-07 09:42:00 +0530

    edited

---
+++ 08.conditional_statements/if.py
@@ -0,0 +1,64 @@
+
+
+##name=input("Enter name: ")
+#name=name.lower().strip()
+#print(name)
+#if name.startswith("s"):
+     #print("Name starts with S!")
+
+
+#age=int(input("Enter your age: ").split()[0])
+#print(age)
+
+
+
+#age=int(input("Enter your age:").split()[0])
+#Gender=input("Enter your Gender:").lower().strip()
+##print(age, Gender)
+#if age>=18:
+    #if Gender=="female":
+       # print("Seat available!")
+    #if Gender!="female":
+        #print("Seat not available")
+
+
+
+# username=input("Enter username: ").split()[0]
+# password=input("Enter password: ")
+# print(username,password)
+# if username=="CodingGita":
+#     if password=="codinggita@12":
+#         print("Login Successful")
+#     else:
+#         print("Incorrect password")
+# else:
+#     print("Incorrect Details")
+
+
+
+# num=int(input("Enter a number:"))
+# if num>0:
+#     if num>100:
+#         print("Greater than 100")
+#     if num<100:
+#         print("Less than 100")
+# else:
+#     print("Not Positive")
+
+
+# num=int(input("Enter number:"))
+# if num!=0:
+#     if num>0:
+#         print("Positive")
+#     else:
+#         print("Negative")
+#     print("Is a non-zero number.")
+# else:
+#     print("Not a non-zero number.")
+
+# age= int(input("enter your age:"))
+# if age<13:
+#     print("Child Ticket")
+# else:
+#     print("Regular Ticket")
+
