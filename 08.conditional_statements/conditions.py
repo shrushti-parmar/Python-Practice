10bd1947b3afe034f7d508292008b04fbaf48db0
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-09-08 09:40:31 +0530

    ediited

--- 08.conditional_statements/conditions.py
+++ 08.conditional_statements/conditions.py
@@ -36,3 +36,12 @@ if name.startswith("S"):
     print("True")
 
 
+is_student = True     
+has_id = True         
+has_ticket = True    
+if is_student and has_id and has_ticket:
+    print("Allowed")
+else:
+    print("Not Allowed")
+
+
