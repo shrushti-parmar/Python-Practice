a1f7eb3c16261aae9f700c992c1b4ccd67b4d511
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-09-09 09:32:31 +0530

    added files

---
+++ Problem_Solving_Assignment/level2.py
@@ -0,0 +1,24 @@
+#11
+
+# year=int(input("Enter year: "))
+# if year%400==0:
+#     print(f"{year} is a leap year.")
+# elif year%4==0 and year%100!=0:
+#     print(f"{year} is a leap year.")
+# else:
+#     print(f"{year} is not a leap year.")
+
+
+
+
+# char=input("Enter a character:").strip()
+# if
+#     print("Uppercase Letter")
+# elif char.lower():
+#     print("Lowercase Letter")
+# elif (char.digit()):
+#     print("Digit")
+# else:
+#     print("Special Character")
+
+
