6e8eee8fedcd2ccd7a765797bec572d201becf8a
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-09-10 09:31:18 +0530

    added files

---
+++ 10.For_Loop/loop.py
@@ -0,0 +1,39 @@
+#1
+for i in range(5):
+    print("Hello")
+
+#2
+# for i in range(1,10):
+#     print(i, end=" ")
+
+#3
+# for i in range(1,11):
+#     print(i, end=" ")
+
+#4
+# for i in range(10,0, -1):
+#     print(i, end=" ")
+
+#5
+# for i in range(5,50,5):
+#     print(i)
+
+#6
+# for i in range(2,20):
+#     if i%2==0:
+#         print(f"{i} is Even")
+#7
+# for i in range(1,19):
+#     if i%2==1:
+#         print(f"{i} is Odd")
+
+#8
+# for i in range(3,21,3):
+#     print(i, end=" ")
+
+#9
+# for i in range(20,0,-2):
+#     print(i, end=" ")
+
+#10
+
