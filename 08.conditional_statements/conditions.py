cf253663d166fd1e8ebb715dfbb350c9fccdf976
shrushti-parmar <shrushti.parmar.cg@gmail.com>
2026-09-10 09:36:20 +0530

    moved demo.py to another folder

--- 08.conditional_statements/conditions.py
+++ 08.conditional_statements/conditions.py
@@ -1,44 +1,44 @@
-num= 20
-if num>10:
-    print("Greater than 10")
+# num= 20
+# if num>10:
+#     print("Greater than 10")
 
-age= 17
-if age>=18:
-    print("Adult")
+# age= 17
+# if age>=18:
+#     print("Adult")
 
-num= int(input("Enter number: "))
-if num>0:
-    print("Positive")
+# num= int(input("Enter number: "))
+# if num>0:
+#     print("Positive")
 
-marks= int(input("Enter marks: "))
-if marks >= 40:
-    print("PASS")
+# marks= int(input("Enter marks: "))
+# if marks >= 40:
+#     print("PASS")
 
-num=int(input("Enter number: "))
-if num==0:
-    print("Zero")
+# num=int(input("Enter number: "))
+# if num==0:
+#     print("Zero")
 
-num=int(input("Enter number: "))
-if num>0:
-    print("POSITIVE")
-else: 
-    print("NOT POSITIVE")
-
-
-num=int(input("Enter age: "))
-if age >= 18:
-    print("Adult")
+# num=int(input("Enter number: "))
+# if num>0:
+#     print("POSITIVE")
+# else: 
+#     print("NOT POSITIVE")
 
 
+# num=int(input("Enter age: "))
+# if age >= 18:
+#     print("Adult")
 
-name= input("Enter name: ")
-if name.startswith("S"):
-    print("True")
 
 
-is_student = True     
-has_id = True         
-has_ticket = True    
+# name= input("Enter name: ")
+# if name.startswith("S"):
+#     print("True")
+
+
+is_student = bool(input("Enter if he is a student: "))     
+has_id = bool(input("Enter if he has id: "))         
+has_ticket = bool(input("Enter if he has ticket: "))  
 if is_student and has_id and has_ticket:
     print("Allowed")
 else:
